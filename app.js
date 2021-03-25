31cbfa7b0e1e9853b3e34cec6018c2b214122f08
Michael Carr <xyeres@me.com>
2021-03-24 17:29:27 -0700

    Error handling

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 const express = require('express');
-const pug = require('pug');
 const routes = require('./routes')
 var path = require('path');
-const { nextTick } = require('process');
 
 const app = express();
 
+// Set view engine and path to views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+// Set static virtual url
 app.use('/static', express.static('public'));
 
+// Set all routes to use routes module
 app.use('/', routes);
 
+// Handle 404 errors
 app.use(function (req, res, next) {
-    const err = new Error('Sorry, that page cannot be found!');
+    const err = new Error('Sorry, that page cannot be found.');
     err.status = 404;
-    err.message = err.message || 'Page not found!';
     next(err);
-})
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+    // Set default error message
+    err.message = err.message || 'An error occurred while trying to process your request.';
+    // Set response status code
+    res.status(err.status || 500);
+    // Log it out
+    console.error(`${err.message} (${err.status})`)
+    // Render a simple error page to user
+    res.send(`<h1>${err.message}</h1> A ${err.status} error occurred <p>${err.stack}</p>`)
+});
 
 app.listen('3000');
